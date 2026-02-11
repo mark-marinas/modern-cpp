5611f84121c0a9f00199e7a1d15f450dc2db399a
markmarinas <mark.marinas@gmail.com>
2026-02-11 22:28:13 +0800

    smart pointers

--- src/smart_pointers.cpp
+++ src/smart_pointers.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <string> 
 
 using namespace std;
 
@@ -10,14 +11,24 @@ class Base {
 };
 
 class Derived: public Base {
+    private:
+        string name;
     public:
-        void print_message() override { cout << "hello from derived" << endl; } 
+        void print_message() override { cout << "hello " << name << endl; } 
+        Derived(string name): name(name) { }
 };
 
 int main()
 {
-    unique_ptr<Base> ptr = make_unique<Derived>();
+    unique_ptr<Base> ptr = make_unique<Derived>("Marla");
     ptr->print_message();
+
+    shared_ptr<Base> oPtr = make_shared<Derived>("Mark");
+    shared_ptr<Base> ptr2 = oPtr; 
+    shared_ptr<Base> ptr3 = ptr2; //this doesnt work on unique ptr because a unique_ptr points to only 1 object.
+    ptr3->print_message();
+    cout << (oPtr == ptr2) << endl;
+
     return 0;
 
 }
