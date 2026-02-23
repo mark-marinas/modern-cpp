a1e1385a3826ec835555a2065e77b6b5c7464d8a
markmarinas <mark.marinas@gmail.com>
2026-02-23 22:09:56 +0800

    Standard library

---
+++ src/standard_library.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <array>
+#include <vector>
+
+using namespace std;
+
+class Name {
+    private:
+        string first, last;
+    public:
+        Name(string fname, string lname): first{fname}, last{lname} {};
+        string get_fullname() { return  first + " " + last; } 
+};
+
+/*
+template<size_t SIZE>
+void print_names(array<Name, SIZE>& n)
+{
+    for (int i=0; i<n.size(); i++) {
+        cout << n.at(i).get_fullname() << endl;
+    }
+}
+*/
+void print_names(vector<Name>& names) {
+    for (Name& name: names) {
+        cout << name.get_fullname() << endl;
+    }
+}
+
+int main()
+{
+    //array<Name, 3> Marinas { Name{"mark", "marinas" }, Name{"marla", "marinas"}, Name{"Mikaela", "Marinas"} };
+    vector<Name> Marinas { Name{"mark", "marinas" }, Name{"marla", "marinas"}, Name{"Mikaela", "Marinas"} }; 
+    print_names(Marinas);
+
+}
