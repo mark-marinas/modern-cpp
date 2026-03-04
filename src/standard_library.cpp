384f2f604099a83091ef34302f0800a10e720d58
markmarinas <mark.marinas@gmail.com>
2026-03-04 21:19:17 +0800

    add vector, map, sorting

--- src/standard_library.cpp
+++ src/standard_library.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <map>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
 
@@ -11,6 +14,11 @@ class Name {
         Name(string fname, string lname): first{fname}, last{lname} {};
         string get_fullname() { return  first + " " + last; } 
 };
+
+template<typename T>
+T sub(T a) {
+    return -1*a;
+}
 
 /*
 template<size_t SIZE>
@@ -33,4 +41,28 @@ int main()
     vector<Name> Marinas { Name{"mark", "marinas" }, Name{"marla", "marinas"}, Name{"Mikaela", "Marinas"} }; 
     print_names(Marinas);
 
+    map<string, int> ages {{"c", 47}, {"a", 21 }, {"v", 46}};
+    for (const auto& age: ages) {
+        cout << age.first << " " << age.second << endl;
+    }
+    cout << endl;
+
+    vector< pair<string, int> > vAges;
+     for (const auto& age: ages) {
+        vAges.push_back(make_pair(age.first, age.second));
+    }   
+
+    sort(vAges.begin(), vAges.end(), []( auto a, auto b  ){ return a.second < b.second; });
+    for (const auto& el: vAges) {
+        cout << el.first << " " << el.second << endl;
+    }
+    cout << endl;
+
+
+    map<string, string> roles { {"father", "mark"}, {"mother", "marla"}, {"daughter", "ambie" } };
+    roles["son"] = "Miguel";
+    if ( roles.find("son") != std::end(roles)) { roles.erase(roles.find("son"));}
+    for (const auto& role: roles) {
+        cout << role.first << "=" << role.second << endl;
+    }
 }
